f2b672f35797496139d7cb0fb80bbced45b5c8d3
Mitsue Stefani <mstefani@il-k5.msk.21-school.ru>
2020-12-04 00:18:27 +0300

    good_rt

--- SDL2-2.0.12/build/SDL_d3dmath.d
+++
@@ -1,7 +0,0 @@
-build/SDL_d3dmath.lo: \
-  /Users/mstefani/RTv_last/SDL2-2.0.12/src/render/SDL_d3dmath.c \
-  /Users/mstefani/RTv_last/SDL2-2.0.12/src/render/../SDL_internal.h \
-  /Users/mstefani/RTv_last/SDL2-2.0.12/src/render/../dynapi/SDL_dynapi.h \
-  /Users/mstefani/RTv_last/SDL2-2.0.12/src/render/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h
--- SDL2-2.0.12/build/SDL_systimer.d
+++
@@ -1,9 +0,0 @@
-build/SDL_systimer.lo: \
-  /Users/mstefani/RTv_last/SDL2-2.0.12/src/timer/unix/SDL_systimer.c \
-  /Users/mstefani/RTv_last/SDL2-2.0.12/src/timer/unix/../../SDL_internal.h \
-  /Users/mstefani/RTv_last/SDL2-2.0.12/src/timer/unix/../../dynapi/SDL_dynapi.h \
-  /Users/mstefani/RTv_last/SDL2-2.0.12/src/timer/unix/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_timer.h include/SDL_stdinc.h \
-  include/SDL_error.h include/SDL_assert.h \
-  /Users/mstefani/RTv_last/SDL2-2.0.12/src/timer/unix/../SDL_timer_c.h
